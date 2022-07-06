31af93073b0d64ee4a4b4a5dc577662f8c440e5e
Maxwell Pirtle <maxwellpirtle@me.com>
2022-07-06 09:21:11 -0400

    Remove redudant `erase()` call
    We were removing threads from the backtrack
    set twice; the second removal always did nothing
    since the thread was just removed!

--- src/MCMINI_Private.cpp
+++ src/MCMINI_Private.cpp
@@ -17,6 +17,8 @@ extern "C" {
     #include <unistd.h>
 }
 
+using namespace std;
+
 /* The shmTransitionTypeInfo resides in shared memory */
 /* The semaphores must also reside in shared memory as per the man page */
 
@@ -110,8 +112,6 @@ mc_scheduler_main()
 
     mc_exhaust_threads(initialTransition);
     mc_exit_with_trace_if_necessary(traceId);
-    // Possibly print
-
     program = mc_enter_gdb_debugging_session_if_necessary(traceId++);
     if (MC_IS_SOURCE_PROGRAM(program))
         return MC_SOURCE_PROGRAM;
@@ -120,15 +120,14 @@ mc_scheduler_main()
     int curTransitionStackDepth = static_cast<int>(programState->getTransitionStackSize());
 
     while (curStateStackDepth > 0) {
+        shared_ptr<MCStateStackItem> sTop = programState->getStateItemAtIndex(curStateStackDepth - 1);
 
-        std::shared_ptr<MCStateStackItem> sTop = programState->getStateItemAtIndex(curStateStackDepth - 1);
         if (sTop->hasThreadsToBacktrackOn()) {
-            // TODO: We can be smart here and only run a thread
-            // if it is not already in a sleep set or lock set (eventually)
-            programState->reflectStateAtTransitionDepth(curTransitionStackDepth - 1);
-
+            
             // DPOR ensures that any thread in the backtrack set is enabled in this state
             tid_t backtrackThread = sTop->popFirstThreadToBacktrackOn();
+
+            programState->reflectStateAtTransitionDepth(curTransitionStackDepth - 1);
             std::shared_ptr<MCTransition> backtrackOperation = programState->getNextTransitionForThread(backtrackThread);
 
             program = mc_enter_gdb_debugging_session_if_necessary(traceId);
