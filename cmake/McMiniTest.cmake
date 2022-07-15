0be1771d3b8432b136f954ea47d9b014a550ac02
Maxwell Pirtle <maxwellpirtle@me.com>
2022-07-15 10:35:55 -0400

    Add GitSubmodule CMake module

--- cmake/McMiniTest.cmake
+++ cmake/McMiniTest.cmake
@@ -5,8 +5,10 @@
 # @brief: Adds a test case for CTest based
 # on the contents of a file containing Google Tests
 #
-# Use this macro to include raw source code
-# 
+# Use this macro to include tests written using
+# Google Test. All tests included in all of the
+# files provided will be combined into a single
+# executable run by CTest
 #
 # @param TESTNAME: The name of the test suite
 # generated for CTest. CTest will run an executable
@@ -18,20 +20,27 @@
 # the files listed following the name of the test
 # suite will be packaged and run as a single
 # executable by CTest
-macro(package_add_test TESTNAME)
+function(add_gtest_suite TESTNAME)
     add_executable(${TESTNAME} ${ARGN})
     target_include_directories(${TESTNAME} ${MCMINI_GOOGLE_TEST_INCLUDE_DIR})
     target_link_libraries(${TESTNAME} gtest gmock gtest_main)
-
     gtest_discover_tests(${TESTNAME}
         WORKING_DIRECTORY ${PROJECT_DIR}
         PROPERTIES VS_DEBUGGER_WORKING_DIRECTORY "${PROJECT_DIR}"
     )
     set_target_properties(${TESTNAME} PROPERTIES FOLDER tests)
-endmacro()
+    
+    if (VERBOSE_TESTING) 
+        message(STATUS "GTest test suite ${TESTNAME} added")
+    endif()
+endfunction()
 
 mark_as_advanced(
     BUILD_GMOCK BUILD_GTEST BUILD_SHARED_LIBS
     gmock_build_tests gtest_build_samples gtest_build_tests
     gtest_disable_pthreads gtest_force_shared_crt gtest_hide_internal_symbols
 )
+
+function(mcmini_add_executable_test TESTNAME EXECUTABLE)
+
+endfunction()
