b4e6e60f9bdfc3a653f758591777429904207735
lukagolf <78548325+lukagolf@users.noreply.github.com>
2022-12-02 00:00:50 +0000

    Make directories follow singular naming & modify test-script accordingly (#62)
    * make directories follow singular naming
    
    * Make test-script work with new naming conventions
    
    * Change scripts to script

---
+++ script/remake.zsh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+/home/luka/Documents/Research/mcmini/scripts/clean.zsh
+/home/luka/Documents/Research/mcmini/scripts/build.zsh
+
+
--- scripts/remake.zsh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-/home/parallels/research/mcmini/scripts/clean.zsh
-/home/parallels/research/mcmini/scripts/build.zsh
-
-
