af90b5a10260111db761baef95b2d03a5ce14ef0
Gene Cooperman <gene@ccs.neu.edu>
2022-12-26 16:41:55 -0500

    Stop McMini test in one hour unless '--long-test'

--- include/mcmini/MCEnv.h
+++ include/mcmini/MCEnv.h
@@ -6,6 +6,7 @@
 #define ENV_PRINT_AT_TRACE         "env_print_at_trace"
 #define ENV_STOP_AT_FIRST_DEADLOCK "env_stop_at_first_deadlock"
 #define ENV_CHECK_FORWARD_PROGRESS "env_check_forward_progress"
+#define ENV_LONG_TEST              "MCMINI_LONG_TEST"
 #define ENV_VERBOSE                "VERBOSE"
 
 #endif // MC_MCENV_H
