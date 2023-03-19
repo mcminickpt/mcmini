0a4ee0abbc0850f7949367d27e762eccb812f1b3
Maxwell Pirtle <maxwellpirtle@me.com>
2023-03-19 09:21:58 +0100

    Add implementation of glibc condition variable
    The group1-group2 implementation of condition
    variables was added in this commit. The implementation
    leverages that of the "default" policy and differs
    specifically in how it decides to generate a
    wake group when it is signaled (first picking
    arbitrarily among group 1 and moving group 2
    into group 1 if necessary)

--- src/misc/cond/MCConditionVariableGLibcPolicy.cpp
+++ src/misc/cond/MCConditionVariableGLibcPolicy.cpp
@@ -4,12 +4,6 @@
 namespace mcmini {
 
 void
-ConditionVariableGLibcPolicy::receive_signal_message()
-{
-  // TODO: Implement this
-}
-
-void
 ConditionVariableGLibcPolicy::receive_broadcast_message()
 {
   ConditionVariableDefaultPolicy::receive_broadcast_message();
@@ -26,17 +20,58 @@ ConditionVariableGLibcPolicy::receive_broadcast_message()
 }
 
 void
+ConditionVariableGLibcPolicy::receive_signal_message()
+{
+  // Threads in group 1, when signaled, are allowed
+  // to escape from the condition variable arbitrarily
+  //
+  // Otherwise, if group 1 is empty, group 2 becomes the
+  // new group 1 and we wake threads arbitrarily as before
+  if (group1.empty()) {
+    this->group1 = this->group2;
+    this->group2.clear();
+  }
+
+  // It's possible that group2 was empty to, in which case there's
+  // nothing to do and the signal goes unconsumed
+  if (!group1.empty()) {
+    this->wake_groups.push_back(
+      WakeGroup(group1.begin(), group1.end()));
+  }
+}
+
+void
 ConditionVariableGLibcPolicy::wake_thread(tid_t tid)
 {
   ConditionVariableDefaultPolicy::wake_thread(tid);
 
-  // TODO: Implement this
+  // Remove the thread from the first group
+  const auto thread_in_group1 =
+    std::find(group1.begin(), group1.begin(), tid);
+
+  // It's possible that the thread is being awoken
+  // but is NOT contained in group1 (e.g. after a
+  // broadcast where the thread is moved into
+  // the set of `broadcast_eligible_threads`).
+  // If it is actually contained in the group
+  if (thread_in_group1 == group1.end()) {
+    group1.erase(thread_in_group1);
+  }
+
+  {
+    // The thread should NOT be in group 2: only threads
+    // from group 1 should be eligible to wake up
+    const auto thread_in_group2 =
+      std::find(group2.begin(), group2.begin(), tid);
+    MC_ASSERT(thread_in_group2 == group2.end());
+  }
 }
 
 void
 ConditionVariableGLibcPolicy::add_waiter(tid_t tid)
 {
-  // TODO: Implement this
+  // New threads enter into group 2 in the glibc scheme
+  this->group2.push_back(tid);
 }
 
 std::unique_ptr<ConditionVariablePolicy>
