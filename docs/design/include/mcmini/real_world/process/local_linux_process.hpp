f51296b8d5df1471afa74b6382e74682b6b530e9
aayushi363 <aayushi363@gmail.com>
2024-02-08 03:16:12 -0500

    memory region configuration

--- docs/design/include/mcmini/real_world/process/local_linux_process.hpp
+++ docs/design/include/mcmini/real_world/process/local_linux_process.hpp
@@ -20,13 +20,16 @@ class local_linux_process : public process {
  private:
   pid_t pid;
 
+
   // TODO: We add the shared memory portion here for now and figure out the
   // "runner" bit later... Each local linux process will share the same static
   // memory region. Even in the runner model, the thread runners would each
   // share the memory region but it wouldn't be attached to the processes
   // themselves
+
   static shared_memory_region read_write_region;
   static mcmini::detail::volatile_mem_stream &get_mem_stream_wrapper() {}
+
 
  public:
   local_linux_process() = default;
@@ -34,5 +37,13 @@ class local_linux_process : public process {
   local_linux_process(pid_t pid) : pid(pid) {}
 
   std::istream &execute_runner(runner_id_t mcmini_runner_id) override;
+
+  // Initialize the shared memory region
+  static void initializeSharedMemory(const std::string& shm_file_name, size_t region_size) {
+    read_write_region = shared_memory_region(shm_file_name, region_size);
+
+  }
+
 };
+
 }  // namespace mcmini::real_world
