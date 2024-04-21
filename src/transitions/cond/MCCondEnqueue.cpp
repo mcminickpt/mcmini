f1ebb4558de0cd28415e4e2b34bf5eb6aaf500b1
Gene Cooperman <gene@ccs.neu.edu>
2024-04-21 17:09:33 -0400

    Recognize PTHREAD_COND_INITIALIZER

--- src/transitions/cond/MCCondEnqueue.cpp
+++ src/transitions/cond/MCCondEnqueue.cpp
@@ -1,111 +1,109 @@
 #include "mcmini/transitions/cond/MCCondEnqueue.h"
 #include "mcmini/MCTransitionFactory.h"
-#include "mcmini/mcmini_private.h"
+#include "mcmini/mcmini_private.h" /* For trace_pid, and state */
 #include "mcmini/transitions/mutex/MCMutexTransition.h"
 #include "mcmini/transitions/mutex/MCMutexUnlock.h"
 #include "mcmini/misc/cond/MCConditionVariableArbitraryPolicy.hpp"
 #include <pthread.h>
 #include <string.h>
+#include <sys/uio.h> /* For process_vm_readv() */
 
 MCTransition *
 MCReadCondEnqueue(const MCSharedTransition *shmTransition,
                   void *shmData, MCStack *state)
 {
-  // FIXME:  MCCondSignal.cpp does:
-  //       const auto condInShm    = static_cast<pthread_cond_t **>(shmData);
-  //   Shouldn't we do it that way also here, instead of
-  //   hiding the design with 'auto'?  - Gene
-  const auto shmCond =
-    static_cast<MCSharedMemoryConditionVariable *>(shmData);
-  const auto condInShm     = shmCond->cond;
-  const auto mutexInShm    = shmCond->mutex;
-  const auto condSystemId  = (MCSystemID)condInShm;
-  const auto mutexSystemId = (MCSystemID)mutexInShm;
-  // We can't freeze this as 'const', since we might nave to init cond later.
+  // WAS: const MCSystemID condSystemId = *condInShm; condInShm=shmData;
+  pthread_cond_t * condSystemId = *static_cast<pthread_cond_t **>(shmData);
+  // WAS: const auto mutexSystemId = (MCSystemID)mutexInShm;
+  auto shmCond = static_cast<MCSharedMemoryConditionVariable *>(shmData);
+  const auto mutexSystemId = (MCSystemID)shmCond->mutex;
+  const auto mutexThatExists =
+    state->getVisibleObjectWithSystemIdentity<MCMutex>(mutexSystemId);
+  // We can't freeze this as 'const', since we might have to init cond later.
   auto condThatExists =
     state->getVisibleObjectWithSystemIdentity<MCConditionVariable>(
       condSystemId);
-  const auto mutexThatExists =
-    state->getVisibleObjectWithSystemIdentity<MCMutex>(mutexSystemId);
 
-  /*** BAD DESIGN:
-   *** McMini has two state variables:  MCStack and mcmini::ConditionVariable::Shadow;.state
-   ***   where using MCConditionVariableShadow = mcmini::ConditionVariable::Shadow
-   *** (You can distinguish them by observinnmg that one uses thpe 'auto' and the other uses tyhpe 'auto'.
-   ***  You just havve to figure out nhhow the two non-equal thpes 'auto' are different.)
-   *** McMmini has two condIn Shm:
-   ***  condInShm2 = static_cast<pthread_cond_t **>(shmData); and condInShm = shmCond->cond;
-   *** (You can distinguish them by observinnmg that one uses thpe 'auto' and the other uses tyhpe 'auto'.
-   ***  You just havve to figure out nhhow the two non-equal thpes 'auto' are different.)
-   ***/
-  // *** We imitate the spaghetti code here, to get down to the 'state' for ConmInit (not CondEnqueue).
-  // *** The XXX2 variables are taken from MCCondinit,cpp (same dir);  XXX2 is used to avoid name conflicts
-  auto condInShm2 = static_cast<pthread_cond_t **>(shmData);
-  auto systemId2  = (MCSystemID)*condInShm2;
-  auto shadow2 = MCConditionVariableShadow(*condInShm2);
-  auto state2 = shadow2.state;
-
-  /*** BAD DESIGN:  mutexInShm->state works; but condInShm->state fails.
-   ***              The code cleverly uses 'auto' to make sure that
-   ***              developers can never be sure about what are the
-   ***              the types for mutexInShm and condInShm.
-   ***              This is the principle of 'data hiding' in OOD.  :-)
-   ***              So, the design rule seems to be: don't extend
-   ***              any code to support 'cond'.
-   ***  We want:  MCConditionVariable->shadow.state, but by using the
-   ***            the principle of 'data hiding', we use 'auto' to make
-   ***            sure that it's impossible to decide which of
-   ***            these variables has that type.
-   ***/
-  if (state2 == MCConditionVariableShadow::undefined) {
-    // Then create and register an MCConditionVariable object.
-    pthread_cond_t cond_initializer = PTHREAD_COND_INITIALIZER;
-    // mutexinShm->systemIdentity, but condInShm already means 'pthread_cond_t *'
-    // (Weird!  Why create a special type for 'pthread_cond_t *' ?)
-    if (memcmp(condInShm, &cond_initializer,
-               sizeof(cond_initializer)) != 0) {
-      memcpy(condInShm, &cond_initializer,
-             sizeof(cond_initializer));
-      mcprintf("WARNING: A mutex wasn't initialized by pthread_cond_init()\n"
-               "* We are initializing it now to PTHREAD_COND_INITIALIZER\n"
-               "* You can ignore this if target code also initialized it to\n"
-               "*   PTHREAD_COND_INITIALIZER\n");
+  bool testIfValidCond = false;
+  // SEE:  src/transitions/cond/MCCondInit.cpp:MCReadCondInit()
+  if (condThatExists == nullptr) {
+    auto shadow = MCConditionVariable::Shadow(condSystemId);
+    // If this has state 'undefined', then the condThatExists is still 'NULL'
+    if (shadow.state == MCConditionVariable::Shadow::undefined) {
+      testIfValidCond = true;
     }
+    // FIXME: Allow dynamic selection of wakeup policies.
+    // For now, we hard-code it here. Not great, but at least
+    // we can change it relatively easily still
 
-    // SEE:  src/transitions/cond/MCCondInit.cpp:MCReadCondInit()
-    if (condThatExists == nullptr) {
-      auto shadow = MCConditionVariableShadow(*condInShm2);
+    // FIXME:  We're assigning a condition variable policy of arbitrary.
+    //         Is this reasonable?
+    auto policy = std::unique_ptr<mcmini::ConditionVariablePolicy>(
+      new mcmini::ConditionVariableArbitraryPolicy());
 
-      // FIXME: Allow dynamic selection of wakeup policies.
-      // For now, we hard-code it here. Not great, but at least
-      // we can change it relatively easily still
+    auto newCond =
+      std::make_shared<mcmini::ConditionVariable>(shadow, std::move(policy));
+    // Then create and register an MCConditionVariable object.
+    state->registerVisibleObjectWithSystemIdentity(condSystemId, newCond);
+    // condThatExists =
+    //   std::static_pointer_cast<MCConditionVariable, MCVisibleObject>(
+    //     newCond);
+    // Get the current cond var on the stack, along with condSystemIdentity
+    //   (the pthread_cond_t struct of the user).
+    condThatExists =
+      state->getVisibleObjectWithSystemIdentity<MCConditionVariable>(
+        condSystemId);
+    // This is still MCConditionVariable::Shadow::undefined;
+    // We set that in the next 'if' statement.
+  }
 
-      // FIXME:  We're assigning a condition variable policy of arbitrary.
-      //         Is tnhis reasonable?
-      auto policy = std::unique_ptr<mcmini::ConditionVariablePolicy>(
-        new mcmini::ConditionVariableArbitraryPolicy());
+  assert(condThatExists != nullptr);
+  assert(mutexThatExists != nullptr);
 
-      auto newCond =
-        std::make_shared<mcmini::ConditionVariable>(shadow, std::move(policy));
-      state->registerVisibleObjectWithSystemIdentity(systemId2, newCond);
-      condThatExists =
-        std::static_pointer_cast<MCConditionVariable, MCVisibleObject>(
-          newCond);
+  if (testIfValidCond || condThatExists -> shadow.state ==
+                         MCConditionVariable::Shadow::undefined) {
+#ifdef HAS_PROCESS_VM
+    pthread_cond_t cond_initializer = PTHREAD_COND_INITIALIZER;
+    // NOTE:  If PTHREAD_COND_INITIALIZER was used on a global variable,
+    //        the macro is all zeros, and the data segment is all zeros.
+    //        So, we can't tell if the initializer was or was not used.
+    //          But if a local variable of type pthread_cond_t is not
+    //        not initialized with PTHREAD_COND_INITIALIZER, and was
+    //        not initialized with pthread_cond_init, then this code can
+    //        truly catch an error.  pthread_cond_signal() would return
+    //        error "Illegal argument", but only if the user had checked
+    //        the return value of pthread_cond_signal, and then the errno.
+    pthread_cond_t local_cond;
+    struct iovec iov_remote_cond =
+      { .iov_base = condSystemId, .iov_len = sizeof(cond_initializer) };
+    struct iovec iov_local_cond =
+      { .iov_base = &local_cond, .iov_len = sizeof(cond_initializer) };
+    process_vm_readv(trace_pid, &iov_local_cond, 1, &iov_remote_cond, 1, 0);
+    if (memcmp(&local_cond, &cond_initializer, sizeof(cond_initializer)) != 0) {
+      mcprintf("\n*** ERROR (pthread_cond_wait/traceId: %d): The cond var\n"
+               "***     was never initialized.\n"
+               "***   Use pthread_cond_init() or PTHREAD_COND_INITIALIZER.\n"
+               "***   Additional McMini traces will not work with this bug.\n"
+               "***   McMini is exiting now.\n\n", traceId);
+      // No need to patch it with process_vm_writev.  We will stop now.
+      mc_stop_model_checking(EXIT_FAILURE);
+    }
+#endif
+    if (condThatExists -> shadow.state ==
+        MCConditionVariable::Shadow::undefined) {
+      condThatExists -> shadow.state = MCConditionVariable::Shadow::initialized;
     }
   }
 
-  MC_REPORT_UNDEFINED_BEHAVIOR_ON_FAIL(
-    condThatExists != nullptr, "Attempting to wait on a condition "
-                               "variable that is uninitialized.");
-  MC_REPORT_UNDEFINED_BEHAVIOR_ON_FAIL(
-    mutexThatExists != nullptr,
-    "Attempting to wait on a condition variable with an "
-    "uninitialized mutex.");
+  assert(condThatExists -> shadow.state ==
+           MCConditionVariable::Shadow::initialized ||
+         condThatExists -> shadow.state ==
+           MCConditionVariable::Shadow::destroyed);
+
   MC_REPORT_UNDEFINED_BEHAVIOR_ON_FAIL(
     mutexThatExists->isLocked(),
-    "Attempting to wait on a condition variable with a "
-    "mutex that is already unlocked.");
-  // FIXME:  This stmt is repeated below.  Shouldn't we delete his one? - Gene
+    "Attempting to wait on a condition variable with a mutex that is unlocked.");
+
   MC_REPORT_UNDEFINED_BEHAVIOR_ON_FAIL(
     !condThatExists->isDestroyed(),
     "Attempting to wait on a destroyed condition variable.");
@@ -117,9 +115,6 @@ MCReadCondEnqueue(const MCSharedTransition *shmTransition,
       "variable is undefined behavior. Ensure that you're calling\n"
       "pthread_cond_wait() with the same mutex.");
   }
-  MC_REPORT_UNDEFINED_BEHAVIOR_ON_FAIL(
-    !condThatExists->isDestroyed(),
-    "Attempting to wait on a destroyed condition variable.");
 
   const auto threadThatRanId = shmTransition->executor;
   const auto mutexAssociatedWithConditionVariable =
@@ -129,13 +124,15 @@ MCReadCondEnqueue(const MCSharedTransition *shmTransition,
   // to NOT already be associated with a mutex at this point.
   // E.g., the first call to pthread_cond_wait() will have a
   // condition variable that isn't associated with a mutex.
+  //    mutexThatExists->mutexShadow.systemIdentity
   if (mutexAssociatedWithConditionVariable) {
     MC_REPORT_UNDEFINED_BEHAVIOR_ON_FAIL(
       *mutexAssociatedWithConditionVariable == *mutexThatExists,
-      "A mutex has already been associated with this condition "
-      "variable. Attempting to wait on a condition variable using "
-      "more "
-      "than one mutex is undefined");
+      "A different mutex was previously bound to this condition variable,\n"
+      "and at least one thread is still blocked on that previous mutex.\n"
+      "But pthread_cond_wait is attempting to bind a new mutex to the\n"
+      "condition variable, even while some thread is still blocked on\n"
+      "the first mutex.");
   }
 
   // NOTE: We have to associate the mutex with the condition
