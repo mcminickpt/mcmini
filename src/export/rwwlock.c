d68a43ed1d9a822b9ad86db690ce887fb573f973
Gene Cooperman <gene@ccs.neu.edu>
2024-06-20 12:45:15 -0400

    export/rwwlock.h warnings fixed; better comment

--- src/export/rwwlock.c
+++ src/export/rwwlock.c
@@ -1,26 +1,27 @@
 #include "mcmini/export/rwwlock.h"
 
 // This proves that McMini behaves as an emulator:
-// we don't even need to define implementations
-// for the true function calls here. As long as
-// McMini has the semantics correct, it will
-// just work!
+// A user can link this code into the target application.  We don't even need
+// to define an implementation for when the target calls the functions here.
+// McMini already models these functions as desired, and will continue
+// to block or unblock threads according to a spec analogous to rwlock
+// for POSIX.  Since McMini has the semantics correct, it will just work!
 
 int
 pthread_rwwlock_init(pthread_rwwlock_t *rwwlock)
-{}
+{ return 0; }
 int
 pthread_rwwlock_rdlock(pthread_rwwlock_t *rwwlock)
-{}
+{ return 0; }
 int
 pthread_rwwlock_wr1lock(pthread_rwwlock_t *rwwlock)
-{}
+{ return 0; }
 int
 pthread_rwwlock_wr2lock(pthread_rwwlock_t *rwwlock)
-{}
+{ return 0; }
 int
 pthread_rwwlock_unlock(pthread_rwwlock_t *rwwlock)
-{}
+{ return 0; }
 int
 pthread_rwwlock_destroy(pthread_rwwlock_t *rwwlock)
-{}
+{ return 0; }
