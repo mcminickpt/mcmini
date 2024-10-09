e3c4a0b7564a468b844b4cb6ac619094867a31ba
Maxwell Pirtle <maxwellpirtle@gmail.com>
2024-10-09 17:03:40 +0200

    Fix broken CMake installation
    The CMake build has been broken for a
    little while. This fixes the issues with
    it. `cmake -B build` and other standard
    CMake commands should work as expected now.

---
+++ config.cmake
@@ -0,0 +1,25 @@
+/* include/config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have process_vm_readv and process_vm_writev. */
+#cmakedefine HAS_PROCESS_VM
+
+/* define if the compiler supports basic C++14 syntax */
+#cmakedefine HAVE_CXX11
+
+/* Define to the address where bug reports for this package should be sent. */
+#cmakedefine PACKAGE_BUGREPORT "@PACKAGE_BUGREPORT@"
+
+/* Define to the full name of this package. */
+#cmakedefine PACKAGE_NAME "@PACKAGE_NAME@"
+
+/* Define to the full name and version of this package. */
+#cmakedefine PACKAGE_STRING "@PACKAGE_STRING@"
+
+/* Define to the one symbol short name of this package. */
+#cmakedefine PACKAGE_TARNAME "@PACKAGE_TARNAME@"
+
+/* Define to the home page for this package. */
+#cmakedefine PACKAGE_URL "@PACKAGE_URL@"
+
+/* Define to the version of this package. */
+#cmakedefine PACKAGE_VERSION "@PACKAGE_VERSION@"
