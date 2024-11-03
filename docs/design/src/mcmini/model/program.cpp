f6c45c54db0e146c0cf97dd0ab121fd22a1f642d
aayushi363 <aayushi363@gmail.com>
2024-11-03 23:15:16 +0000

    Condition variable wrappers and objects added

--- docs/design/src/mcmini/model/program.cpp
+++
@@ -1,8 +0,0 @@
-#include "mcmini/model/program.hpp"
-
-using namespace model;
-
-program::program(state &&initial_state,
-                 pending_transitions &&initial_first_steps)
-    : next_steps(std::move(initial_first_steps)),
-      state_seq(std::move(initial_state)) {}
