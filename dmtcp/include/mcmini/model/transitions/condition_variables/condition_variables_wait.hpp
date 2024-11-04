00fb8a19c462482a449b1bdc9fe794cb87e3d507
aayushi363 <aayushi363@gmail.com>
2024-11-04 18:17:39 +0000

    Translate recorded cv to model side

---
+++ dmtcp/include/mcmini/model/transitions/condition_variables/condition_variables_wait.hpp
@@ -0,0 +1,64 @@
+#pragma once
+
+#include "mcmini/model/objects/condition_variables.hpp"
+#include "mcmini/model/objects/mutex.hpp"
+#include "mcmini/model/transition.hpp"
+#include "mcmini/model/transitions/condition_variables/condition_variables_signal.hpp"
+#include "mcmini/model/transitions/condition_variables/condition_variables_init.hpp"
+
+namespace model {
+namespace transitions {
+
+struct condition_variable_wait : public model::transition {
+ private:
+  const state::objid_t cond_id; /* The condition variable this transition waits on */
+  const state::objid_t mutex_id; /* The mutex this transition waits on */
+
+ public:
+  condition_variable_wait(runner_id_t executor, state::objid_t cond_id, state::objid_t mutex_id)
+      : transition(executor), cond_id(cond_id), mutex_id(mutex_id) {}
+  ~condition_variable_wait() = default;
+
+  status modify(model::mutable_state& s) const override {
+    using namespace model::objects;
+
+    // Retrive the state of the condition variable and the associated mutex
+    const condition_variable* cv = s.get_state_of_object<condition_variable>(cond_id);
+    const mutex* m = s.get_state_of_object<mutex>(mutex_id);
+
+    // A `condition_variables_wait` cannot be applied if mutex is not locked
+    if (!m->is_locked()) {
+      return status::disabled;
+    }
+
+    if (cv->is_initialized() || cv->is_signalled()) {
+      // If the condition variable is initialized or signalled, we move to the transitional state
+      s.add_state_for_obj(cond_id, new condition_variable(condition_variable::cv_transitional));
+    } else {
+      // If the condition variable is uninitialized, we initialize it
+      s.add_state_for_obj(cond_id, new condition_variable(condition_variable::cv_initialized));
+    }
+
+    // Increment the waiting count of the condition variable
+    int current_waiting_count = cv->get_waiting_count();
+    s.add_state_for_obj(cond_id, new condition_variable(condition_variable::cv_waiting, current_waiting_count + 1));
+    return status::exists;
+  }
+  state::objid_t get_id() const { return this->cond_id; }
+  std::string to_string() const override {
+    return "pthread_cond_wait(cond:" + std::to_string(cond_id) + ", mutex:" + std::to_string(mutex_id) + ")";
+  }
+
+  // MARK: Model checking functions
+    bool depends(const condition_variable_init* ci) const {
+        return this->cond_id == ci->get_id();
+    }
+    bool depends(const condition_variable_wait* cw) const {
+        return this->cond_id == cw->get_id();
+    }
+    bool coenabled_with(const condition_variable_signal* cs) const {
+        return this->cond_id != cs->get_id();
+    }
+};
+} // namespace transitions
+} // namespace model
---
+++ dmtcp/src/mcmini/model/transitions/condition_variables.cpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "mcmini/misc/extensions/unique_ptr.hpp"
+#include "mcmini/model/visible_object_state.hpp"
+
+namespace model {
+namespace objects {
+
+struct condition_variable : public model::visible_object_state {
+  
+};
+    
+    }  // namespace objects
+    }  // namespace model
