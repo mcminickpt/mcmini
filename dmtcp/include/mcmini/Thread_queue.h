535e4d3b34879995bde2f33652225340018f0047
aayushi363 <aayushi363@gmail.com>
2025-03-31 23:48:53 +0000

    changes to implement per thread cv state tracking

--- dmtcp/include/mcmini/Thread_queue.h
+++ dmtcp/include/mcmini/Thread_queue.h
@@ -2,11 +2,13 @@
 #define THREAD_QUEUE_H
 #include <pthread.h>
 #include <stdbool.h>
+#include "spy/checkpointing/cv_status.h" // For condition_variable_status
 #include "mcmini/defines.h"
 
 
 typedef struct thread_queue_node {
     runner_id_t thread;
+    condition_variable_status thread_cv_state;
     struct thread_queue_node *next;
 } thread_queue_node;
 
@@ -19,11 +21,15 @@ typedef struct thread_queue {
 thread_queue_node* create_thread_queue_node();
 thread_queue* create_thread_queue();
 void init_thread_queue(thread_queue *queue);
-void enqueue_thread(thread_queue *queue, runner_id_t thread);
+void enqueue_thread(thread_queue *queue, runner_id_t thread , condition_variable_status cv_state);
 runner_id_t dequeue_thread(thread_queue *queue);
 runner_id_t peek_thread(thread_queue *queue);
 bool is_in_thread_queue(thread_queue *queue, runner_id_t thread);
 bool is_queue_empty(thread_queue *queue);
+condition_variable_status get_thread_cv_state(thread_queue *queue, runner_id_t thread);
+void update_thread_cv_state(thread_queue *queue, runner_id_t thread, condition_variable_status new_state);
+runner_id_t get_waiting_thread_node(thread_queue *queue);
 void print_thread_queue(const thread_queue *queue);
+int remove_thread_from_queue(thread_queue* queue, runner_id_t tid) ;
 
 #endif // THREAD_QUEUE_H
