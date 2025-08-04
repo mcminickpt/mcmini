ce2b1ce5de0a07d9ca12a6953839e65eea7fca40
aayushi363 <aayushi363@gmail.com>
2025-08-04 22:39:33 +0000

    Avoid race b/w read/write shm_global_variable
    Added a function to write shm_global_variable address which is protected
    by the mutexes. Another function to read shm_global_variable with a
    thread local variable to make sure it read only once. This way we avoid
    the race happening between reading and writing of this shared global
    variable.

--- dmtcp/src/common/shm_config.c
+++ dmtcp/src/common/shm_config.c
@@ -14,3 +14,26 @@ void mc_get_shm_handle_name(char *dst, size_t sz) {
   snprintf(dst, sz, "/mcmini-%s-%lu", getenv("USER"), (long)getppid());
   dst[sz - 1] = '\0';
 }
+
+// Define the global variable and initialize the mutex
+volatile void* global_shm_start = NULL;
+pthread_mutex_t shm_start_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+// Function to set the global shared memory start address
+void set_global_shm_start(void* addr) {
+    pthread_mutex_lock(&shm_start_mutex);
+    global_shm_start = addr;
+    pthread_mutex_unlock(&shm_start_mutex);
+}
+
+// Function to get the global shared memory start address
+volatile void* get_global_shm_start() {
+    // Use a thread-local variable to cache the value
+    static __thread volatile void* local_shm_start = NULL;
+
+    if (local_shm_start == NULL) {
+        local_shm_start = global_shm_start;
+    }
+
+    return local_shm_start;
+}
